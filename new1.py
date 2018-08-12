169f7dd7d585de61c88b0860e1d46ff999c06048
Aksh-a1 <akshmourya@gmail.com>
2018-08-12 15:12:35 +0530

    made progress

--- new1.py
+++ new1.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, Response
+from flask import Flask, request, Response, render_template
 import time
 
 PATH_TO_TEST_IMAGES_DIR = './templates/f'
@@ -6,15 +6,22 @@ PATH_TO_TEST_IMAGES_DIR = './templates/f'
 app = Flask(__name__)
 
 @app.route('/')
-def ind():
-    return Response(open('./templates/indexx.html').read(), mimetype="text/html")
-
-@app.route('/facecapture')
 def index():
-    return Response(open('./templates/facecapture.html').read(), mimetype="text/html")
+    return render_template('index.html')
+
+@app.route('/register')
+def ind():
+    return render_template('register.html')
+
+@app.route('/facecapture', methods=['GET','POST'])
+def inde():
+    if request.method == 'POST':
+        result = request.form
+        print(result)
+    return render_template('facecapture.html')
 
 # save the image as a picture
-@app.route('/image', methods=['POST'])
+@app.route('/image', methods=['GET','POST'])
 def image():
 
     i = request.files['image']  # get the image
